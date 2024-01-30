326a20268b2e228fb6780b46d078807bf2a07753
Anand Gora <goraanand8058@gmail.com>
2024-01-30 15:54:52 +0530

    First Commint

---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logosec">
+        <div className="logo">Led Ads</div>
+        <i className="fa-solid fa-bars"></i>
+      </div>
+
+      <div className="message">
+        <div className="dp">
+          <i className="fa-solid fa-user"></i>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
